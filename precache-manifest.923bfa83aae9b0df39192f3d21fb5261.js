090c2c46e799f6011e85fc51180e2c7eb3d14451
Anuj Gupta <junagupta19@gmail.com>
2020-03-14 16:49:23 +0530

    Updates

---
+++ precache-manifest.923bfa83aae9b0df39192f3d21fb5261.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66ec7d09f60f9aa7dc36",
+    "url": "/React-Portfolio/static/css/main.828b38a4.chunk.css"
+  },
+  {
+    "revision": "66ec7d09f60f9aa7dc36",
+    "url": "/React-Portfolio/static/js/main.66ec7d09.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React-Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "aa67d2827e505520bf1c06adbf9600a1",
+    "url": "/React-Portfolio/index.html"
+  }
+];
