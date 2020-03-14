a685f993b461d770e59d86d4ff8ae0aff77c15dc
Anuj Gupta <junagupta19@gmail.com>
2020-03-14 16:03:52 +0530

    Made changes to support personal portfolio

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
 export default class ContactUs extends Component {
   render() {
-    let resumeData = this.props.resumeData;
+    let resumeData = this.props.resumeData
     return (
-      <section id="contact">
-          <div className="row section-head">
-            <div className="ten columns">
-              <p className="lead">
-              Feel free to contact me for any work or suggestions below
-              </p>
-            </div>
+      <section id='contact'>
+        <div className='row section-head'>
+          <div className='ten columns'>
+            <p className='lead'>
+              Feel free to contact me at anujgupta019@gmail.com
+            </p>
           </div>
-          <div className="row">
+        </div>
+        {/* <div className="row">
             <aside className="eigth columns footer-widgets">
               <div className="widget">
                 <h4>Linked in :
@@ -19,8 +19,8 @@ export default class ContactUs extends Component {
                 </h4>
               </div>
             </aside>
-          </div>
-        </section>
-        );
+          </div> */}
+      </section>
+    )
   }
 }
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,154 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: 'https://rbhatia46.github.io/',
+  name: 'Anuj Gupta',
+  role: 'Full Stack Web and Mobile Developer',
+  linkedinId: 'anuj-gupta-9264b9b8',
+  skypeid: 'Your skypeid',
+  roleDescription:
+    'I am a tech enthusiast who loves learning new technologies and creating products using them.',
+  socialLinks: [
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/in/anuj-gupta-9264b9b8/',
+      className: 'fa fa-linkedin'
+    },
+    {
+      name: 'github',
+      url: 'https://github.com/aj019',
+      className: 'fa fa-github'
+    }
+  ],
+  aboutme:
+    "Experienced Javascript developer with expertise in the frameworks of ReactJS - a powerful framework to build interactive UI's",
+  address: 'India',
+  website: 'anujguptawork@gmail.com',
+  education: [
+    {
+      UniversityName: 'Guru Gobind Singh Indraprastha University',
+      specialization: 'BTech in Computer Science',
+      MonthOfPassing: 'June',
+      YearOfPassing: '2017'
+      // Achievements: 'Some Achievements'
+    }
+  ],
+  work: [
+    {
+      CompanyName: 'Tripshire',
+      specialization: 'Full Stack Developer',
+      MonthOfLeaving: 'Present',
+      YearOfLeaving: '',
+      Achievements:
+        'Helped build a B2B platform that streamlines the day to day workflow of a travel agent and increases productivity'
+    },
+    {
+      CompanyName: 'CampK12',
+      specialization: 'Software Developer',
+      MonthOfLeaving: 'June',
+      YearOfLeaving: '2019',
+      Achievements:
+        'Helped build the next web-based AR/VR platform to create realistic user experiences using A-Frame and Three.js on the front-end through HTML5 Canvas, along with the Express/Node.js backend for users. My role involved programming the website (https://hatchxr.com) and the workspace (https://hatchxr.com) using React, Redux, Immutable and similar web technologies.'
+    },
+    {
+      CompanyName: 'YourClothier',
+      specialization: 'Android Developer',
+      MonthOfLeaving: 'Dec',
+      YearOfLeaving: '2017',
+      Achievements: `Mobile Application Development on Android devices for Nationwide distribution \n
+        • Architected / Developed Nationwide Messaging(Notification) app with user segmentation \n
+        • Worked extensively with Activities, Fragments, Services, Notifications \n
+        • Interfaced with Google API's like Maps, Databases, Google Cloud Messaging \n
+        • Web Services implementation with in-app conversion of data into JSON objects \n\n
+        
+        Experience decompiling existing Applications (apk) to understand images layouts so as to enhance the developer experience \n\n
+        
+        Released Applications onto the Google Play Store \n\n
+        
+        Apps are designed with Materials Design in mind when creating custom user interfaces.`
+    },
+    {
+      CompanyName: 'Finocial',
+      specialization: 'Android Intern',
+      MonthOfLeaving: 'Apr',
+      YearOfLeaving: '2017',
+      Achievements:
+        'Worked on various android apps based on client requirements.'
+    },
+    {
+      CompanyName: 'CareerCo',
+      specialization: 'Web Development Intern',
+      MonthOfLeaving: 'Aug',
+      YearOfLeaving: '2015',
+      Achievements:
+        'B2B web application on Data analytics to group raw data and offer services to other business organisations. \n It is created using MVC framework for PHP (Codeignitor) and Bootsrtrap framework for front end development.'
+    },
+    {
+      CompanyName: 'Rutugo',
+      specialization: 'Web Development Intern',
+      MonthOfLeaving: 'July',
+      YearOfLeaving: '2015',
+      Achievements:
+        '-Front end development using HTML5, CSS3 and javascript \n -Back end development using PHP'
+    }
+  ],
+  skillsDescription: 'Your skills here',
+  skills: [
+    {
+      skillname: 'HTML5',
+      skillLevel: '80%' // skillVele out of 100%
+    },
+    {
+      skillname: 'CSS',
+      skillLevel: '80%' // skillVele out of 100
+    },
+    {
+      skillname: 'Reactjs',
+      skillLevel: '80%' // skillVele out of 100
+    }
+  ],
+  portfolio: [
+    {
+      name: 'Catchphrase',
+      description:
+        'Catchphrase is the fun and hilarious guessing game that Jimmy Fallon plays on the Tonight Show with Jimmy Fallon. It has more than 250K+ downloads',
+      imgurl:
+        'https://lh3.googleusercontent.com/beVus6c6IlX20KkuXOKnCQR71-WKBqdbf5j0NbLUooTmELV2YQcqPL-rHXBpUbbYYA=s180-rw',
+      link:
+        'https://play.google.com/store/apps/details?id=com.androidmate.catchphrase'
+    },
+    {
+      name: 'HatchXR',
+      description:
+        'HatchXR is a platform that allows anyone to create VR/AR experiences in minutes',
+      imgurl: 'images//portfolio/hatchxr.png',
+      link: 'https://hatchxr.com'
+    },
+    {
+      name: 'Wikisupply',
+      description:
+        'B2B platform that allows travel agents with DMCs and automates their day to day workflow',
+      imgurl: 'images//portfolio/wikisupply.png',
+      link: 'https://wikisupply.com'
+    },
+    {
+      name: 'Password',
+      description:
+        'Password Game is the fun and hilarious guessing game. The goal of the game Password is to get your partner to guess the password by giving him a one-word and only one-word clue.',
+      imgurl:
+        'https://lh3.googleusercontent.com/OBdMSuDGvZMC2PWcTTMPNvlbrxCNZelXyyMN_QTuePpz3Bypxvc845wG2mcJabyczu8=s180-rw',
+      link:
+        'https://play.google.com/store/apps/details?id=in.androidmate.password_game'
+    }
+  ],
+  testimonials: [
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy'
+    },
+    {
+      description: 'This is a sample testimonial',
+      name: 'Some technical guy'
+    }
+  ]
+}
+
+export default resumeData
